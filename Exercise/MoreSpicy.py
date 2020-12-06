d05afa9a754fadf95cd1b9fd407851c6f7ce6f5b
Aaron Lee <aaronlab.net@gmail.com>
2020-12-06 22:54:20 +0900

    More Spicy

--- Exercise/MoreSpicy.py
+++ Exercise/MoreSpicy.py
@@ -1,9 +1,23 @@
 def solution(scoville, K):
+    import heapq
+    data = []
+
+    for s in scoville:
+        heapq.heappush(data, s)
+        print(s)
+        print(data)
+
     answer = 0
 
-    scoville = scoville
-
-    return answer
+    while len(data) > 0:
+        if data[0] >= K:
+            return answer
+        a = heapq.heappop(data)
+        if data:
+            b = heapq.heappop(data)
+            heapq.heappush(data, a + (b * 2))
+        answer += 1
+    return -1
 
 
 print(solution([1, 2, 3, 9, 10, 12], 7))
