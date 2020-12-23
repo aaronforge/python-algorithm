359a650f361792fc642c478dba490ea23d2c1a84
Aaron Lee <aaronlab.net@gmail.com>
2020-12-23 23:25:03 +0900

    Recap

--- 02.Greedy/02BigNumber.py
+++ 02.Greedy/02BigNumber.py
@@ -1,15 +1,30 @@
+# 5 8 3
+# 2 4 5 4 6
+# ==> 46
+
+# 6 6 6 5 / 6 6 6 5
+
+# 7 7 2
+# 1 2 3 4 5 6 7
+# ==> 47
+# 7 7 6 / 7 7 6 / 7
+
 n, m, k = map(int, input().split())
 numbers = list(map(int, input().split()))
-result = 0
+answer = 0
 
 numbers.sort()
-first_biggest = numbers[n - 1]
-second_biggest = numbers[n - 2]
+big = numbers[n - 1]
+small = numbers[n - 2]
 
-max_count = int(m / (k + 1)) * k
-max_count += m % (k + 1)
+# 수열 길이 = k + 1
+# 수열 반복 횟수 = m / 수열 길이
+# 가장 큰 수 반복 수 = (수열 반복 횟수 * k) + 수열 반복 횟수의 나머지
+# 두번째로 큰 수 반복 수 = m - 가장 큰 수 반복 수
 
-result += max_count * first_biggest
-result += (m - max_count) * second_biggest
+bigCount = ((m // (k + 1)) * k) + (m % (k + 1))
 
-print(result)
+answer += bigCount * big
+answer += (m - bigCount) * small
+
+print(answer)
