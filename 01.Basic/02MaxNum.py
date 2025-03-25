e82e5b9ccbbd2f48144afb8b375aef564edbfa9d
Aaron Lee <aaronlee9303@gmail.com>
2025-03-25 22:20:13 +0900

    clean repository

--- 01.Basic/02MaxNum.py
+++
@@ -1,5 +0,0 @@
-def get_max_num(a, b, c):
-    return max([a, b, c])
-
-
-print(get_max_num(1, 2, 3))
--- Recap/03NumberCardGame.py
+++
@@ -1,9 +0,0 @@
-m, n = map(int, input().split())
-
-answer = 0
-
-for i in range(m):
-    cards = list(map(int, input().split()))
-    answer = max(answer, min(cards))
-
-print(answer)
