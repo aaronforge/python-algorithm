81c644941036bee7cec1d741ea5b969d28787594
Aaron Lee <aaronlee9303@gmail.com>
2025-03-29 22:04:58 +0900

    recap: 음료수 얼려 먹기

--- 7.py
+++ 7.py
@@ -4,32 +4,34 @@ input = sys.stdin.readline
 n, m = map(int, input().split())
 graph = [list(map(int, input().strip())) for _ in range(n)]
 
-def dfs(x: int, y: int):
-    # 탐색 불가
-    if x >= n or x < 0 or y >= m or y < 0:
+
+def dfs(x, y):
+    # 범위 벗어남
+    if x < 0 or x >= n or y < 0 or y >= m:
         return False
 
-    # 미탐색 영역
+    # 칸막이가 존재하지 않아야 함
     if graph[x][y] == 0:
+        # 이미 탐색 완료한 칸
         graph[x][y] = 1
 
-        # 값이 0인 미탐색 상하좌우 좌표를 돌면서
-        # 0이 아닌 값이 나올 때 까지 합칠 수 있는 영역 탐색
-        dfs(x - 1, y)
+        # 현재 칸 기준으로 상하좌우 재귀를 돌면서 연결된 0인 칸 모두 탐색 완료 처리
+        dfs(x -1, y)
         dfs(x + 1, y)
         dfs(x, y - 1)
         dfs(x, y + 1)
 
+        # 구멍이 뚫려있던 칸
         return True
-    
+
     return False
+
 
 result = 0
 
 for x in range(n):
     for y in range(m):
-        # 한 번 들어가서 확장 가능한 영역을 모두 탐색하므로
-        # x, y 좌표에 대해 한번씩만 돌고 오면 됨
+        # 구멍이 뚫린 영역이 존재하는 칸
         if dfs(x, y) == True:
             result += 1
 
