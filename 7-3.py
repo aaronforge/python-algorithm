de7e20a511af8e78b93df5f491181f2ef8de4a77
Aaron Lee <aaronlee9303@gmail.com>
2025-03-31 20:41:39 +0900

    이진탐색 bisect

--- 7-3.py
+++ 7-3.py
@@ -14,3 +14,9 @@ def binary_search(array: list, target: int, start: int, end: int):
 array = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18]
 
 print(binary_search(array, 4, 0, len(array) - 1))
+
+
+import bisect
+
+index = bisect.bisect_left(array, 4)
+print(array[index])
