bbe7652eca808557fdaa54d02fda57057e258520
Aaron Lee <aaron@myorange.io>
2025-04-04 22:01:51 +0900

    미래 도시

--- 9-3.py
+++ 9-3.py
@@ -11,21 +11,23 @@ m = int(input())
 # 그래프 초기화
 graph = [[INF] * (n + 1) for _ in range(n + 1)]
 
+# 자기 자신 비용 초기화
 for i in range(1, n + 1):
-    for j in range(1, n + 1):
-        if i == j:
-            graph[i][j] = 0
+    graph[i][i] = 0
 
 for _ in range(m):
     # A 노드 -> B 노드, 비용 C
     a, b, c = map(int, input().split())
     graph[a][b] = c
 
+# 최단거리 계산
 for k in range(n + 1):
     for i in range(n + 1):
         for j in range(n + 1):
+            # 점화식
             graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
 
+# 결과 출력
 for i in range(1, n + 1):
     for j in range(1, n + 1):
         d = graph[i][j]
