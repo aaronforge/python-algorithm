3127d624aa6cc17cb06ad24593a3ec741b1623c8
Aaron Lee <aaron@myorange.io>
2025-04-08 16:32:25 +0900

    곱하기 혹은 더하기 개선

--- 11-2.py
+++ 11-2.py
@@ -1,4 +1,5 @@
 import sys
+from functools import reduce
 
 
 input = lambda: sys.stdin.readline().rstrip()
@@ -6,12 +7,7 @@ input = lambda: sys.stdin.readline().rstrip()
 # 입력(string으로 받아야 함, 첫 자리가 0일 수 있음)
 s = input()
 
-# 결과 최댓값
-result = 0
-
 # 최댓값 연산 선택해서 결과 업데이트
-for char in s:
-    num = int(char)
-    result = max(result + num, result * num)
+result = reduce(lambda x, y: max(x + int(y), x * int(y)), s, 0)
 
 print(result)
