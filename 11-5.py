f2340f5c2be329bb387d4e1a52f86fb4af891a6e
Aaron Lee <aaronlee9303@gmail.com>
2025-04-10 22:22:18 +0900

    N + M^2

--- 11-5.py
+++ 11-5.py
@@ -4,18 +4,19 @@ import sys
 input = lambda: sys.stdin.readline().rstrip()
 
 
-# N = 공의 수, M = 최대 무게
 n, m = map(int, input().split())
-
-# 볼링공별 무게
 data = list(map(int, input().split()))
+
+# 무게별 볼링공 수
+weight_count = [0] * (m + 1)
+
+for weight in data:
+    weight_count[weight] += 1
 
 result = 0
 
-for i in range(len(data)):
-    for j in data[i:]:
-        if data[i] != j:
-            result += 1
-        
+for i in range(1, m + 1): # 현재 무게 수 index
+    for j in range(i + 1, m + 1): # 현재 무게 다음 무게 수 부터 index
+        result += weight_count[i] * weight_count[j] # 경우의 수 구하기
 
 print(result)
