4e143960a286f71183bdbd3f9a08d5ed27e6d7cc
Aaron Lee <aaronlee9303@gmail.com>
2025-05-07 22:05:03 +0900

    recap: 1이 될 때까지

--- recap/3-4.py
+++ recap/3-4.py
@@ -2,12 +2,15 @@ n, k = map(int, input().split())
 
 result = 0
 
-while n > 1:
-    if n % k == 0:
-        n //= k
-    else:
-        n -= 1
-    
+while n >= k:
+    # 나눈 몫에 k를 곱하면 최대 배수
+    target = (n // k) * k
+    result += (n - target)
+    n = target
+
     result += 1
+    n //= k
+
+result += (n - 1)
 
 print(result)
