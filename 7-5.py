b41fbb00a8e1dfc8a264b1678090b78ee3fe0378
Aaron Lee <aaron@myorange.io>
2025-05-28 09:46:14 +0900

    이진탐색

---
+++ 7-5.py
@@ -0,0 +1,29 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+n = int(input())
+data = list(map(int, input().split()))
+data.sort()
+
+m = int(input())
+req = list(map(int, input().split()))
+req.sort()
+
+def binary_search(array, target, start, end):
+    if start > end:
+        return None
+    
+    mid = (start + end) // 2
+
+    if array[mid] == target:
+        return mid
+    elif array[mid] > target:
+        return binary_search(array, target, start, mid - 1)
+    else:
+        return binary_search(array, target, mid + 1, end)
+
+for r in req:
+    if binary_search(data, r, 0, len(data) - 1):
+        print("yes", end=" ")
+    else:
+        print("no", end=" ")
